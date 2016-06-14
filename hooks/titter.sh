2ee4f3d6839411d2d2051b9b74702e074a9a623e
Michael Bilenko <denso.ffff@gmail.com>
2016-06-14 22:10:45 +0300

    fix production floder

--- hooks/titter.sh
+++ hooks/titter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 OLDPORTS=( `docker ps | grep wriodockerproduction_titter_1 | awk '{print $1}'` )
-cd /compose
+cd /wriodockerproduction
 docker pull webrunes/titter-wrio-app && docker-compose restart titter
