bb3397704104c12eafe19143a092bb3d351ddb36
Michael Bilenko <denso.ffff@gmail.com>
2016-06-14 22:04:54 +0300

    Fix hooks

--- hooks/titter.sh
+++ hooks/titter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 OLDPORTS=( `docker ps | grep wriodockerproduction_titter_1 | awk '{print $1}'` )
-cd /srv/docker/Wrio-Docker-Production
+cd /srv/compose
 docker pull webrunes/titter-wrio-app && docker-compose restart titter
